81bfcd0b2d189b4f293f5f76462c9b53b40504a7
Leon Benić <64423174+BenicLeon@users.noreply.github.com>
2025-03-09 11:08:30 +0100

    Added project structure

---
+++ Vehicle/Vehicle.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vehicle.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
